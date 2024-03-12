8b3e1d3691c1927c3bc2d5b7ebc4c7fa823cf8a9
Harsh-Bansal-13 <111892307+Harsh-Bansal-13@users.noreply.github.com>
2024-03-12 20:53:58 +0530

    Update index.js

--- Server/index.js
+++ Server/index.js
@@ -18,7 +18,8 @@ app.use(express.json());
 app.use(
   cors({
     // origin: ["http://localhost:3000"],
-    origin: ["https://fruits-wala-version-2-1-frontend.vercel.app"],
+    // origin: ["https://fruits-wala-version-2-1-frontend.vercel.app"],
+    origin: ["https://fruits-wala.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
