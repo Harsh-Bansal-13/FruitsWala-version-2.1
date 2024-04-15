43e566d82c814fa01891d2881216525b3a02cb90
Harsh-Bansal-13 <111892307+Harsh-Bansal-13@users.noreply.github.com>
2024-04-15 11:30:17 +0530

    Update index.js

--- Server/index.js
+++ Server/index.js
@@ -142,7 +142,6 @@ app.get("/logout", (req, res) => {
 });
 
 app.get("/getItems", (req, res) => {
-  console.log("Harsh");
   ItemModel.find()
     .then((items) => res.json(items))
     .catch((err) => res.json(err));
